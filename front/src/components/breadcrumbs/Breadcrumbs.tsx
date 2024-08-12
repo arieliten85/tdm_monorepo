c3e79c8d39973b8f904c78eae2d3c086d6abaa00
arielferen <arieliten85@gmail.com>
2024-08-12 12:38:31 -0300

    add pading breadscrum

--- front/src/components/breadcrumbs/Breadcrumbs.tsx
+++ front/src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,28 +1,24 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import "./breadcrumb.scss";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './breadcrumb.scss';
 
 export const Breadcrumb: React.FC = () => {
   const location = useLocation();
-  const pathnames = location.pathname.split("/").filter((x) => x);
+  const pathnames = location.pathname.split('/').filter(x => x);
 
   return (
-    <nav className="breadcrumb">
+    <nav className="breadcrumb pt-4">
       <ul>
         <li>
           <Link to="/">Inicio</Link>
         </li>
         {pathnames.map((value, index) => {
           const decodedValue = decodeURIComponent(value);
-          const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+          const to = `/${pathnames.slice(0, index + 1).join('/')}`;
           const isActive = index === pathnames.length - 1;
           return (
-            <li key={to} className={isActive ? "active" : ""}>
-              {isActive ? (
-                <span>{decodedValue}</span>
-              ) : (
-                <Link to={to}>{decodedValue}</Link>
-              )}
+            <li key={to} className={isActive ? 'active' : ''}>
+              {isActive ? <span>{decodedValue}</span> : <Link to={to}>{decodedValue}</Link>}
             </li>
           );
         })}
